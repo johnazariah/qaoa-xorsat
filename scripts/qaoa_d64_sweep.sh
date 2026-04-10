411f899a3abac243b858584d6d483a843ee487d3
John S Azariah <john.azariah@gmail.com>
2026-04-11 09:11:37 +1000

    fix: --mem=1448G for current cluster nodes

--- scripts/qaoa_d64_sweep.sh
+++ scripts/qaoa_d64_sweep.sh
@@ -40,7 +40,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=28
-#SBATCH --mem=2700G
+#SBATCH --mem=1448G
 #SBATCH --requeue
 #SBATCH --comment="maintain_node"
 #SBATCH --output=qaoa-d64_%A-%a.out
